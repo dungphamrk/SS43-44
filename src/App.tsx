2de38e27fafb13f914423cb46dcdc36714a0863c
dungphamrk <147359437+dungphamrk@users.noreply.github.com>
2024-05-27 12:14:53 +0700

    SS43+44

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminDashboard from './components/Admin'
+import Login from './components/Login'
+
+export default function App() {
+  return (
+    <div>
+      <AdminDashboard/>
+      <Login/>
+    </div>
+  )
+}
